95071c4035fbe2cc9c91d5a6f128b21093623662
Dedelene <voicudiana66@gmail.com>
2026-04-07 15:50:49 +0300

    found 339 technologies

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import parquet from 'parquetjs-lite';
+import Wappalyzer from 'wappalyzer';
 
 async function citireParquet() {
     const domenii = [];
@@ -9,15 +10,41 @@ async function citireParquet() {
         while(record = await cursor.next()){
             domenii.push(record.root_domain);
         }
+        await reader.close();
         return domenii;
     } catch (error) {
         console.error('Error reading Parquet file:', error);
     }
 }
 
+async function fetchTehnologii(url){
+        const wappalyzer = new Wappalyzer();
+    try {
+        await wappalyzer.init();
+        const site = await wappalyzer.open("https://" + url);
+        const rezultate = await site.analyze();
+        await wappalyzer.destroy();
+
+        return rezultate.technologies.map(tech => tech.name);
+    } catch (error) {
+        
+        console.error(`Eroare la scanarea domeniului ${url}:`, error.message);
+        await wappalyzer.destroy();
+        return [];
+    }
+    
+}
+
 async function main() {
     const domenii = await citireParquet();
-    console.log(domenii);
+    const tehnologiiUnice = new Set();
+    for(const domeniu of domenii){
+        const tehnologiiGasite = await fetchTehnologii(domeniu);
+        for(const tech of tehnologiiGasite) {
+            tehnologiiUnice.add(tech);
+        }
+        console.log(tehnologiiUnice.size);
+    }
 }
 
 main();
